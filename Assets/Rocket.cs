9bd28f6660515f61101c7b70a4047ea5d1da37f7
Derek Haus <rumbleincybertron@gmail.com>
2019-02-24 23:06:41 +0900

    Rigid Body and Upward Thrust

---
+++ Assets/Rocket.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))    // Can thrust while rotating
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating Left");
+            if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.Space))
+            {
+                print("Thrusting and rotating left");
+            }
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Right");
+            if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.Space))
+            {
+                print("Thrusting and rotating right");
+            }
+        }
+    }
+}
