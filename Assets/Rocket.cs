c8a0e5fcebb8f6ea79c0f910f92834b275654d62
Derek Haus <rumbleincybertron@gmail.com>
2019-02-26 19:46:56 +0900

    Corrected Thrust and Rotation Calculations

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 1000f;
+
     Rigidbody rigidBody;
     AudioSource rocketAudio;
 
@@ -24,9 +27,11 @@ public class Rocket : MonoBehaviour
 
     private void Thrust()
     {
+        float thrustThisFrame = mainThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.Space))    // Can thrust while rotating
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
             if (!rocketAudio.isPlaying)      // So it doesn't layer
             {
                 rocketAudio.Play();
@@ -43,14 +48,15 @@ public class Rocket : MonoBehaviour
     {
         rigidBody.freezeRotation = true; // take manual control of rotation
 
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.back);
+            transform.Rotate(Vector3.back * rotationThisFrame);
         }
 
         rigidBody.freezeRotation = false; // resume physics control of rotation
